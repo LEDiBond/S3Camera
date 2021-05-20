010b4e202f70a06b704ab50d41495af3312c623f
AutobotZee <autoltzeeshan>
2021-05-20 16:44:35 +0200

    Working on Camera capture properties and streaming function.
    S3 class attribute client used insted of resources.
    
    Changed upload method. Now using client service
    
    Made a function to download the current image from cloud.

---
+++ key_intr.py
@@ -0,0 +1,10 @@
+import cv2
+while True:
+    try:
+        #Interupt = input("Stream interrupted : \n Enter 'Stop' to Stop streaming \n Enter 'C' to Continue streaming")
+        #print(Interupt)
+        print("Up for stream")
+        print("Press Ctrl + c to halt the stream")
+    except KeyboardInterrupt:
+        break
+print("Stream stopped ")
